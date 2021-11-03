83c5bdcc1ec73c06338f3c16245d580f55d251b5
Bia10 <t.locke1234@gmail.com>
2021-11-03 23:44:59 +0100

    Add project files.

---
+++ AmeisenBotX.Bootstrap/Launcher.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace AmeisenBotX.Bootstrap
+{
+    public class Launcher
+    {
+        private static string _exePath;
+        private readonly string _cmdArgs;
+        private readonly string _workDirPath;
+
+        public bool Finished { get; private set; }
+        public readonly List<string> NormalOutput;
+        public readonly List<string> ErrorOutput;
+
+        public Launcher(string exePath, string cmdArgs)
+        {
+            _exePath = exePath;
+            _cmdArgs = cmdArgs;
+            NormalOutput = new List<string>();
+            ErrorOutput = new List<string>();
+        }
+
+        public Launcher(string exePath, string cmdArgs, string workDirPath)
+        {
+            _exePath = exePath;
+            _cmdArgs = cmdArgs;
+            _workDirPath = workDirPath;
+            NormalOutput = new List<string>();
+            ErrorOutput = new List<string>();
+        }
+
+        public void Launch()
+        {
+            if (string.IsNullOrEmpty(_exePath))
+                throw new InvalidOperationException("Path to exe null or empty!");
+
+            var workDirPath = _workDirPath;
+            if (string.IsNullOrEmpty(workDirPath))
+            {
+                workDirPath = new FileInfo(_exePath).DirectoryName;
+                if (workDirPath == null)
+                    throw new InvalidOperationException("Failed to obtain path to working directory!");
+            }
+
+            try
+            {
+                var process = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = _exePath,
+                        Arguments = _cmdArgs,
+                        WorkingDirectory = workDirPath,
+                        UseShellExecute = true,
+                        RedirectStandardOutput = false,
+                        RedirectStandardError = false,
+                    }
+                };
+
+                if (process.StartInfo.UseShellExecute == false)
+                {
+                    process.OutputDataReceived += OutputHandler;
+                    process.ErrorDataReceived += ErrorHandler;
+                }
+
+                if (process.Start())
+                {
+                    if (process.StartInfo.UseShellExecute == false)
+                    {
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+                    }
+
+                    process.WaitForExit();
+
+                    if (process.HasExited) Finished = true;
+                }
+                else if (process.Start() && process.HasExited && process.ExitCode != 0)
+                {
+                    throw new Exception($"Process exited after starting. ExitCode: {process.ExitCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occurred during process launch!", ex);
+            }
+        }
+
+        private void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            if (!string.IsNullOrEmpty(outLine.Data))
+                NormalOutput.Add(outLine.Data);
+        }
+
+        private void ErrorHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            if (!string.IsNullOrEmpty(outLine.Data))
+                ErrorOutput.Add(outLine.Data);
+        }
+    }
+}
---
+++ AmeisenBotX.Bootstrap/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AmeisenBotX.Bootstrap
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string trinityBuildPath = "C:\\Build\\bin\\RelWithDebInfo\\";
+            LaunchTrinity(trinityBuildPath);
+        }
+
+        private static void LaunchTrinity(string trinityBuildPath)
+        {
+            var authServerPath = trinityBuildPath + "authserver.exe";
+            var worldServerPath = trinityBuildPath + "worldserver.exe";
+
+            if (File.Exists(authServerPath))
+            {
+                Console.WriteLine("Launching auth server!");
+
+                var launcher = new Launcher(authServerPath, string.Empty, trinityBuildPath);
+                launcher.Launch();
+
+                if (launcher.ErrorOutput.Any())
+                    throw new Exception("Errors occurred during launch of authserver.exe!");
+
+                Console.WriteLine("Auth server ready!");
+            }
+            else
+            {
+                throw new FileNotFoundException($"Failed to find authserver.exe! at path: {authServerPath}");
+            }
+
+            if (File.Exists(worldServerPath))
+            {
+                Console.WriteLine("Launching world server!");
+
+                var launcher = new Launcher(worldServerPath, string.Empty, trinityBuildPath);
+                launcher.Launch();
+
+                if (launcher.ErrorOutput.Any())
+                    throw new Exception("Errors occurred during launch of authserver.exe!");
+
+                Console.WriteLine("World server ready!");
+            }
+            else
+            {
+                throw new FileNotFoundException($"Failed to find worldserver.exe! at path: {worldServerPath}");
+            }
+        }
+    }
+}
